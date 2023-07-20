bb8ff1a0c83f4ff161ad01ddec5e645ca027f657
thandinhjs <dinhcongthan03@gmail.com>
2023-07-20 10:16:33 +0700

    redux-thunk

--- src/app/store.tsx
+++ src/app/store.tsx
@@ -1,12 +1,12 @@
-import { legacy_createStore as createStore, combineReducers } from 'redux'
+import { legacy_createStore as createStore, combineReducers, applyMiddleware } from 'redux'
 import counterReducer from '../reducer/counter';
 import productReducer from '../reducer';
-
+import thunk from 'redux-thunk';
 
 const rootReducer = combineReducers({
     counter: counterReducer,
     products: productReducer
 })
 
-const store = createStore(rootReducer)
+const store = createStore(rootReducer, applyMiddleware(thunk))
 export default store;
--- src/components/productList/index.tsx
+++ src/components/productList/index.tsx
@@ -1,60 +1,23 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { instance } from "../../instances/instance";
+import { addProduct, deleteProduct, fetchProducts, updateProduct } from "../../actions/product";
+import { Dispatch } from "redux";
 const ProductList = () => {
-    const dispatch = useDispatch();
+    const dispatch: Dispatch<any> = useDispatch();
     const { products } = useSelector((state: any) => state.products);
 
     useEffect(() => {
-        const fetchProducts = async () => {
-            try {
-                const data = await instance.get("/products");
-                console.log({ data });
-                dispatch({ type: "products/fetchProducts", payload: data });
-            } catch (error: any) {
-            } finally {
-            }
-        };
-        fetchProducts();
+        dispatch(fetchProducts());
     }, []);
-    const addProduct = async (product: any) => {
-        try {
-            const data = await instance.post("/products", product);
-            dispatch({ type: "products/addProduct", payload: data });
-        } catch (error: any) {
-        } finally {
-        }
-    };
-    const updateProduct = async (product: any) => {
-        try {
-            const data = await instance.put(`/products/${product.id}`, product);
-            dispatch({ type: "products/updateProduct", payload: data });
-            console.log("update data :", data);
 
-        } catch (error: any) {
-            console.log(error);
-
-        }
-    };
-    const deleteProduct = async (id: any) => {
-        try {
-            const data = await instance.delete("/products/" + id);
-            dispatch({ type: "products/deleteProduct", payload: id });
-            console.log("data delete :", data);
-
-
-        } catch (error: any) {
-        } finally {
-        }
-    };
     return (
         <div>
             {products.map((product: any) => (
                 <div key={product.id}>{product.name}</div>
             ))}
-            <button onClick={() => addProduct({ name: "Product ADDed" })}>Thêm</button>
-            <button onClick={() => updateProduct({ name: "Productupdate hihihuhuhuhih", id: 3 })}>Cập nhật</button>
-            <button onClick={() => deleteProduct(3)}>delete</button>
+            <button onClick={() => dispatch(addProduct({ name: "Product ADDed" }))}>Thêm</button>
+            <button onClick={() => dispatch(updateProduct({ name: "Product update", id: 3 }))}>Cập nhật</button>
+            <button onClick={() => dispatch(deleteProduct(3))}>delete</button>
         </div>
     );
 };
