917c660f85f87046420186801208b459483a8300
thandinhjs <dinhcongthan03@gmail.com>
2023-08-25 21:03:45 +0700

    inint

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,14 @@ import ProductUpdate from "./components/admin/products/ProductUpdate"
 import ListCate from "./components/admin/categories/ListCate"
 import AddCate from "./components/admin/categories/AddCate"
 import UpdateCate from "./components/admin/categories/UpdateCate"
+import Home from "./components/layout/clients/Main/Home"
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Outlet />}>
-
+          <Route index element={<Home />} />
         </Route>
         <Route path="/admin" element={<Outlet />}>
           <Route index element={<ProductList />} />
